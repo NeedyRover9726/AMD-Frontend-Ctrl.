14bc0fe0830576e078b06926dfb08e06c2519f21
NeedyRover9726 <89884077+NeedyRover9726@users.noreply.github.com>
2026-07-11 14:23:42 +0800

    Update color.kt

--- kotlin+java/com.example.ctrl/ui.theme/color.kt
+++ kotlin+java/com.example.ctrl/ui.theme/color.kt
@@ -2,16 +2,8 @@ package com.example.ctrl.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
-
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
-
 // --- YOUR CUSTOM APP COLORS ---
-val BgMidnight = Color(0xFF0F0F1A)
+val BgMidnight = Color(0xFF0F0A1E)
 val TopLogoBg = Color(0xFF1A1A2E)
 val CardDarkPurple = Color(0xFF2D004D)
 
