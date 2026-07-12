e7d8f58a2eeafa3d320e34c59d4d1310df3885d8
NeedyRover9726 <89884077+NeedyRover9726@users.noreply.github.com>
2026-07-13 02:56:38 +0800

    Add files via upload

---
+++ build.gradle.kts
@@ -0,0 +1,6 @@
+plugins {
+    // This perfectly matches the [plugins] section of the TOML file!
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.kotlin.android) apply false
+    alias(libs.plugins.kotlin.compose) apply false
+}
